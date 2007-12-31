3df0e3edd5443dbc51e97741517904dfbe56808e
Pavel N. Krivitsky <pavel@uow.edu.au>
2007-12-31 05:38:34 +0000

    Fixed a stupid error in predict.ergmm.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@498 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/predict.ergmm.R
+++ R/predict.ergmm.R
@@ -2,20 +2,20 @@ predict.ergmm<-function(object,...,which.par="post"){
   if(class(which.par)=="ergmm.par"){
     which.par<-which.par
   }else if(which.par=="start"){
-    which.par<-ergmm.fit$start
+    which.par<-object$start
   }else if(which.par=="mle"){
-    which.par<-ergmm.fit$mle
+    which.par<-object$mle
   }else if(which.par=="pmean"){
-    which.par<-summary(ergmm.fit,point.est=c("pmean"))$pmean
+    which.par<-summary(object,point.est=c("pmean"))$pmean
   }else if(which.par=="mkl"){
-    which.par<-ergmm.fit$mkl
+    which.par<-object$mkl
   }else if(which.par=="pmode"){
-    which.par<-ergmm.fit$pmode
+    which.par<-object$pmode
   }else if(which.par=="post"){
-    return(with(ergmm.fit,post.predict.C(model,samples,control)))
+    return(with(object,post.predict.C(model,samples,control)))
   }else if(is.numeric(which.par) && round(which.par)==which.par){
-    which.par<-ergmm.fit$samples[[which.par]]
+    which.par<-object$samples[[which.par]]
   }else stop("Invalid parameter structure.")
 
-  ergmm.EY(ergmm.fit$model,which.par)
+  ergmm.EY(object$model,which.par)
 }
