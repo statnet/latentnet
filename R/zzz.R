6eadfcb8068d5f5c388750be7f7d96976f2410e3
Pavel Krivitsky <pavel@uow.edu.au>
2008-01-15 20:22:59 +0000

    Cleaned up zzz.R and added a .Last.lib function.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@555 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/zzz.R
+++ R/zzz.R
@@ -16,33 +16,14 @@
 #
 ######################################################################
 
-.conflicts.OK <- "0.6-1"
-
 .First.lib <- function(lib, pkg){
     library.dynam("latentnet", pkg, lib)
     
     ## Remember where we loaded this instance of latentnet, so that
     ## the snowFT slave functions could do the same.
-    library(tools)
     assign("path.to.me",file_path_as_absolute(lib),pos="package:latentnet")
     assign("nlog.double.eps",-log(.Machine$double.eps),pos="package:latentnet")
     
-    if(R.version$major=="1"){
-     ehelp <- help(package="latentnet")$info[[2]][[2]]
-     cat(paste("'",ehelp[4],"'\n",
-               "Version ",ehelp[2],
-               " created on ",ehelp[3],".\n", sep=""))
-    }else{
-     if(R.version$minor < "1.0"){
-      ehelp <- library(help="latentnet",lib.loc=NULL,character.only=TRUE)$info[[2]]
-     }else{
-      ehelp <- library(help="latentnet",lib.loc=NULL,character.only=TRUE)$info[[1]]
-     }
-     cat(paste(substring(ehelp[4],first=16),"\n",
-               "Version ",substring(ehelp[2],first=16),
-               " created on ",
-                substring(ehelp[3],first=16),".\n", sep=""))
-    }
     cat(paste(
 "copyright (c) 2003-2007, Pavel N. Krivitsky, University of Washington\n",
 "                         Mark S. Handcock, University of Washington\n",
@@ -51,5 +32,8 @@
 "                         Peter Hoff, University of Washington\n",sep=""))
     cat('See http://www.csde.washington.edu/statnet\n')
     cat('Type help(package="latentnet") to get started.\n')
-    require(network, quietly=TRUE)
+}
+
+.Last.lib <- function(libpath){
+  library.dynam.unload("latentnet",libpath)
 }
