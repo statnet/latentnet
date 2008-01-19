04f6b6c508317d87e1ae833effcb74074eb99dd7
Pavel Krivitsky <pavel@uow.edu.au>
2008-01-19 03:54:59 +0000

    Updated citation, description, and attribution information.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@575 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,3 @@
-######################################################################
-#
-# zzz.r
-#
-# copyright (c) 2003-2007, Pavel N. Krivitsky, University of Washington
-#                          Mark S. Handcock, University of Washington
-#                          David R. Hunter, Penn State University
-#                          Carter T. Butts, University of California - Irvine
-#                          Martina Morris, University of Washington
-# written December 2003 edited afterwards
-# Licensed under the GNU General Public License version 2 (June, 1991)
-#
-# Part of the latentnet statnet package
-#
-# .First.lib is run when the package is loaded with library(latentnet)
-#
-######################################################################
-
 .First.lib <- function(lib, pkg){
     library.dynam("latentnet", pkg, lib)
     
@@ -23,15 +5,22 @@
     ## the snowFT slave functions could do the same.
     assign("path.to.me",file_path_as_absolute(lib),pos="package:latentnet")
     assign("nlog.double.eps",-log(.Machine$double.eps),pos="package:latentnet")
+
+    ehelp <- library(help="ergm",lib.loc=lib,character.only=TRUE)$info[[1]]
+    cat(paste(substring(ehelp[4],first=16),"\n",
+              "Version ",substring(ehelp[2],first=16),
+              " created on ",
+              substring(ehelp[3],first=16),".\n", sep=""))
     
     cat(paste(
-"copyright (c) 2003-2007, Pavel N. Krivitsky, University of Washington\n",
+"copyright (c) 2003-2008, Pavel N. Krivitsky, University of Washington\n",
 "                         Mark S. Handcock, University of Washington\n",
-"                         Susan Shortreed, University of Washington\n",
-"                         Jeremy Tantrum, University of Washington\n",
-"                         Peter Hoff, University of Washington\n",sep=""))
-    cat('See http://www.csde.washington.edu/statnet\n')
-    cat('Type help(package="latentnet") to get started.\n')
+"                         and others; see LICENSE for the full list of\n",
+"                         contributors.\n"
+              ,sep=""))
+    cat('This package is part of statnet project (http://www.statnetproject.org).\n')
+    cat('For citation information type `citation("latentnet")\'.\n')
+    cat('Type `help(package="latentnet")\' to get started.\n')
 }
 
 .Last.lib <- function(libpath){
