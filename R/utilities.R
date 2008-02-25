a72b16693888300f82c13f2ac7ca7bd9a0904378
Pavel Krivitsky <pavel@uow.edu.au>
2008-02-25 03:49:06 +0000

    Removed the unused sociomatrix(...) function (and its documentation) and deleted the now-replaced plot.dens.ergmm.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@663 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/utilities.R
+++ R/utilities.R
@@ -17,24 +17,6 @@ is.latent.cluster<-function(x) inherits(x,"ergmm") && x$model$d>0 && x$model$G>0
 is.sender<-function(x) inherits(x,"ergmm") && x$model$sender
 is.receiver<-function(x) inherits(x,"ergmm") && x$model$receiver
 
-sociomatrix<-function(object, matrix.type="adjacency", attrname=NULL)
-{
-  if(class(object)=="network.series") {
-    object <- object$networks[[1]]
-  }
-  if(is.network(object)){
-    x <- as.matrix.network(object,matrix.type=matrix.type, attrname=attrname)
-    xnames <- network.vertex.names(object)
-    dimnames(x) <- list(xnames,xnames)
-    if(is.bipartite(object)){
-     nevents <- is.bipartite(object)
-     nactors <- network.size(object) - nevents
-     x <- x[1:nactors, (1:nevents)+nactors]
-    }
-    x
-  }
-}
-
 extraneous.argcheck<-function(...){
   ## Because #$^$% R wants various functions implementing generics to
   ## functions to take ..., which is wonderful for missing spelling
