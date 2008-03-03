d427baa47437a08c83b82527838f36935e89c667
Pavel N. Krivitsky <pavel@uow.edu.au>
2008-03-03 08:41:15 +0000

    Split ergmm.sample.utils into several files.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@703 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ R/procrustes.R
@@ -0,0 +1,24 @@
+proc.Z.mean.C<-function(sample,Z.ref,center=FALSE,verbose=0){
+  n<-dim(Z.ref)[1]
+  G<-dim(sample$Z.mean)[2]
+  if(is.null(G)) G<-0
+  d<-dim(Z.ref)[2]
+  S<-dim(sample$Z)[1]
+  ## Center Z.ref.
+  Z.ref<-scale(Z.ref,scale=FALSE)
+
+  Cret<-.C("procr_transform_wrapper",
+           S=as.integer(S),
+           n=as.integer(n),
+           d=as.integer(d),
+           G=as.integer(G),
+           Z.ref=as.double(Z.ref),
+           Z=as.double(sample$Z),
+           Z.mean=as.double(sample$Z.mean),
+           verbose=as.integer(verbose),
+           PACKAGE="latentnet")
+  sample$Z<-if(d>0)array(Cret$Z,dim=c(S,n,d))
+  sample$Z.mean<-if(G>0)array(Cret$Z.mean,dim=c(S,G,d))
+  
+  sample
+}
