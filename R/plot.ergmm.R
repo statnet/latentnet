64620352c0f03812cd2ac43ac54ef1d980688e63
Pavel N. Krivitsky <pavel@uow.edu.au>
2008-03-11 01:29:54 +0000

    plot.ergmm can now use rgl to make 3D latent space plots.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@721 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/plot.ergmm.R
+++ R/plot.ergmm.R
@@ -1,19 +1,25 @@
-plot.ergmm <- function (x, ..., vertex.cex=1, vertex.sides=16*ceiling(sqrt(vertex.cex)),
-                          what="mkl",
-                          main = NULL, xlab=NULL, ylab=NULL, xlim=NULL,ylim=NULL,
-                          object.scale=formals(plot.network.default)$object.scale,
-                          pad=formals(plot.network.default)$pad,
-                          cluster.col=c("red","green","blue","cyan","magenta","orange","yellow","purple"),
-                          vertex.col=NULL, print.formula=TRUE,
-                          edge.col=8,
-                          pie = FALSE,
-                          labels=FALSE,
-                          rand.eff=NULL,
-                          plot.means=TRUE,plot.vars=TRUE,
-                          suppress.axes=FALSE,
-                          jitter1D=1,curve1D=TRUE,suppress.center=FALSE,
-                          density.par=list())
-{
+plot3d.ergmm<-function(x,...){
+  plot.ergmm(x,rgl=TRUE,...)
+}
+
+plot.ergmm <- function(x, ..., vertex.cex=1, vertex.sides=16*ceiling(sqrt(vertex.cex)),
+                       what="mkl",
+                       main = NULL, xlab=NULL, ylab=NULL, zlab=NULL, xlim=NULL,ylim=NULL, zlim=NULL,
+                       object.scale=formals(plot.network.default)$object.scale,
+                       pad=formals(plot.network.default)$pad,
+                       cluster.col=c("red","green","blue","cyan","magenta","orange","yellow","purple"),
+                       vertex.col=NULL, print.formula=TRUE,
+                       edge.col=8,
+                       pie = FALSE,
+                       labels=FALSE,
+                       rand.eff=NULL,
+                       plot.means=TRUE,plot.vars=TRUE,
+                       suppress.axes=FALSE,
+                       jitter1D=1,curve1D=TRUE,
+                       use.rgl=FALSE,
+                       vertex.3d.cex=1/20,
+                       suppress.center=FALSE,
+                       density.par=list()){
 
   ## For convenience...
   Yg<-x$model$Yg
@@ -24,6 +30,8 @@ plot.ergmm <- function (x, ..., vertex.cex=1, vertex.sides=16*ceiling(sqrt(verte
   G<-x$model$G
   if(G<1) pie<-FALSE
 
+  if(use.rgl) require(rgl)
+  
   ## Set default axis labels.
   if(d==1){
     if (is.null(xlab)) 
@@ -35,6 +43,20 @@ plot.ergmm <- function (x, ..., vertex.cex=1, vertex.sides=16*ceiling(sqrt(verte
       xlab <- expression(Z[1])
     if (is.null(ylab)) 
       ylab <- expression(Z[2])
+  }else if(d==3 && use.rgl){
+    if (is.null(xlab)) 
+      xlab <- expression(Z[1])
+    if (is.null(ylab))
+      ylab <- expression(Z[2])
+    if (is.null(zlab)) 
+      zlab <- expression(Z[3])
+  }else if(d>3 && use.rgl){
+    if (is.null(xlab)) 
+      xlab <- "First principal component of Z"
+    if (is.null(ylab)) 
+      ylab <- "Second principal component of Z"
+    if (is.null(zlab)) 
+      zlab <- "Third principal component of Z"
   }else if(d>2){
     if (is.null(xlab)) 
       xlab <- "First principal component of Z"
@@ -218,7 +240,7 @@ plot.ergmm <- function (x, ..., vertex.cex=1, vertex.sides=16*ceiling(sqrt(verte
                     deparse(substitute(x)),sep="")
   }else stop("Invalid latent space position estimate type.")
 
-  ## Transform coordinates for dimensions other than 2.
+  ## Transform coordinates for dimensions other than 2 (or 3, when using rgl).
   if(d==1){    
     Z.pos<-coords.1D(Z.pos,curve1D,jitter1D)
     Z.mean<-coords.1D(Z.mean,curve1D,jitter1D)
@@ -226,13 +248,18 @@ plot.ergmm <- function (x, ..., vertex.cex=1, vertex.sides=16*ceiling(sqrt(verte
       distances<-as.matrix(dist(Z.pos))
       distances<-distances/max(distances)
     }
-  } else if(d>2){ ## I.e. high latent dimension.
+  }else if(d>3 && use.rgl){
+    ## Plot the first three principal components.
+    prc<-prcomp(Z.pos)
+    Z.pos<-predict(prc,Z.pos)[,1:3]
+    Z.mean<-predict(prc,Z.mean)[,1:3]
+  }else if(d>2 && !use.rgl){ ## I.e. high latent dimension.
     ## Plot the first two principal components.
     prc<-prcomp(Z.pos)
     Z.pos<-predict(prc,Z.pos)[,1:2]
     Z.mean<-predict(prc,Z.mean)[,1:2]
   }
-
+  
   ## Set default vertex color.
   if(is.null(vertex.col)){
     if(is.latent.cluster(x) && !pie)
@@ -262,73 +289,92 @@ plot.ergmm <- function (x, ..., vertex.cex=1, vertex.sides=16*ceiling(sqrt(verte
   xylim<-ergmm.plotting.region(Z.pos,if(plot.means) Z.mean,if(plot.vars) Z.var,!suppress.center,pad)
   if(is.null(xlim)) xlim<-xylim$xlim else xylim$xlim<-xlim
   if(is.null(ylim)) ylim<-xylim$ylim else xylim$ylim<-ylim
+  if(is.null(zlim)) zlim<-xylim$zlim else xylim$zlim<-zlim
+
+  if(!use.rgl){
+    ## Go forth, and plot the network.
+    old.warn<-options()$warn
+    options(warn=-1)
     
-  ## Go forth, and plot the network.
-  old.warn<-options()$warn
-  options(warn=-1)
+    plot.network(Yg,coord=Z.pos,
+                 main=main,
+                 vertex.col=if(is.null(vertex.col)) 0 else vertex.col,
+                 vertex.cex=vertex.cex,
+                 xlab=xlab,
+                 ylab=ylab,
+                 xlim=xlim,
+                 ylim=ylim,
+                 object.scale=object.scale,
+                 pad=pad,
+                 suppress.axes=suppress.axes,
+                 vertex.sides=16*sqrt(vertex.cex),
+                 jitter=FALSE,
+                 usecurve=curve1D,
+                 edge.curve=distances,
+                 displaylabels=labels&&!(x$model$d==1 && curve1D==TRUE),
+                 tick=!(x$model$d==1 && curve1D==TRUE),
+                 edge.col=edge.col,
+                 ...)
   
-  plot.network(Yg,coord=Z.pos,
-               main=main,
-               vertex.col=if(is.null(vertex.col)) 0 else vertex.col,
-               vertex.cex=vertex.cex,
-               xlab=xlab,
-               ylab=ylab,
-               xlim=xlim,
-               ylim=ylim,
-               object.scale=object.scale,
-               pad=pad,
-               suppress.axes=suppress.axes,
-               vertex.sides=16*sqrt(vertex.cex),
-               jitter=FALSE,
-               usecurve=curve1D,
-               edge.curve=distances,
-               displaylabels=labels&&!(x$model$d==1 && curve1D==TRUE),
-               tick=!(x$model$d==1 && curve1D==TRUE),
-               edge.col=edge.col,
-               ...)
-  
-  options(warn=old.warn)
-  
-  ## For 1D plots, only plot horizontal axis ticks.
-  if(x$model$d==1 && curve1D==TRUE) {
-    axis(1)
+    options(warn=old.warn)
+  }else{
+    plot3d(Z.pos,type="s",col=if(is.null(vertex.col)) 0 else vertex.col,radius=vertex.cex*vertex.3d.cex,xlab=xlab,ylab=ylab,zlab=zlab,xlim=xlim,ylim=ylim,zlim=zlim,alpha=1,main=main)
   }
 
-  ## Add the model formula as a subtitle.
-  if(print.formula){
-    fmla <- x$model$formula   
-    xformula <- paste(fmla[2],fmla[1],fmla[-c(1:2)],collapse=" ")
-    if(!is.null(x$model$response)) xformula<-paste(xformula,"   (",x$model$response,")",sep="")
-    title(main = xformula, line = 1, cex.main = 0.7)
+  if(!use.rgl){
+    ## For 1D plots, only plot horizontal axis ticks.
+    if(x$model$d==1 && curve1D==TRUE) {
+      axis(1)
+    }
+    
+    ## Add the model formula as a subtitle.
+    if(print.formula){
+      fmla <- x$model$formula   
+      xformula <- paste(fmla[2],fmla[1],fmla[-c(1:2)],collapse=" ")
+      if(!is.null(x$model$response)) xformula<-paste(xformula,"   (",x$model$response,")",sep="")
+      title(main = xformula, line = 1, cex.main = 0.7)
   }
-  
-  ## Plot pie charts.
-  if(pie){
-    piesize<-rep(ergmm.plotting.vertex.radius(vertex.cex,xylim,object.scale),length=n)
-    pie.order<-order(piesize,decreasing=TRUE)
-    for(i in 1:n){
-      ergmm.drawpie(Z.pos[pie.order[i],],piesize[pie.order[i]],Z.pZK[pie.order[i],],n=50,cols=cluster.col)
+    
+    ## Plot pie charts.
+    if(pie){
+      piesize<-rep(ergmm.plotting.vertex.radius(vertex.cex,xylim,object.scale),length=n)
+      pie.order<-order(piesize,decreasing=TRUE)
+      for(i in 1:n){
+        ergmm.drawpie(Z.pos[pie.order[i],],piesize[pie.order[i]],Z.pZK[pie.order[i],],n=50,cols=cluster.col)
+      }
+    }
+
+    ## Mark the events with "bullets" (small circles).
+    if(is.bipartite(Yg)){
+      bip<-Yg %n% "bipartite"
+      points(Z.pos[-(1:bip),],pch=20,cex=vertex.cex[-(1:bip)],col=1)
     }
   }
-
-  ## Mark the events with "bullets" (small circles).
-  if(is.bipartite(Yg)){
-    bip<-Yg %n% "bipartite"
-    points(Z.pos[-(1:bip),],pch=20,cex=vertex.cex[-(1:bip)],col=1)
-  }
-
+  
   ## Mark the center.
-  if(!suppress.center)
-    points(cbind(0,0),pch="+")
-  Z.mean<-if(G>0)Z.mean else cbind(0,0)
+  if(!suppress.center){
+    if(!use.rgl)
+      points(cbind(0,0),pch="+")
+    else
+      tetrahedra3d(0,0,0,color="black",size=1*vertex.3d.cex)
+  }
+  Z.mean<-if(G>0)Z.mean else cbind(0,0,if(use.rgl)0)
 
   ## Mark the cluster means.
-  if(plot.means)
-    points(Z.mean,pch="+",col=cluster.col)
+  if(plot.means){
+    if(!use.rgl)
+      points(Z.mean,pch="+",col=cluster.col)
+    else
+      tetrahedra3d(Z.mean,color=cluster.col,size=1*vertex.3d.cex)
+  }
 
   ## Plot the cluster standard deviations.
-  if(plot.vars)
+  if(plot.vars){
+    if(!use.rgl)
       symbols(Z.mean,circles=sqrt(Z.var),fg=cluster.col,inches=FALSE,add=TRUE,asp=1)
+    else
+      spheres3d(Z.mean,radius=sqrt(Z.var),color=cluster.col,alpha=0.2)
+  }
   
   invisible(Z.pos)
 }
@@ -344,6 +390,7 @@ coords.1D<-function(Z,curve1D,jitter1D){
 }
 
 ergmm.plotting.region<-function(Z.pos,Z.mean,Z.var,include.center,pad){
+  d3=(dim(Z.pos)[2]>=3)
   xylim<-t(apply(rbind(Z.pos+pad,Z.pos-pad,
                        Z.mean+pad,Z.mean-pad,
                        if(!is.null(Z.mean) && !is.null(Z.var)) Z.mean+sqrt(Z.var)+pad,
@@ -355,22 +402,56 @@ ergmm.plotting.region<-function(Z.pos,Z.mean,Z.var,include.center,pad){
                        ),2,range))
   xlim<-xylim[1,]
   ylim<-xylim[2,]
+  if(d3)zlim<-xylim[3,]
 
-  ### Shamelessly stolen from Carter's plot.network.default code.
+  ### Shamelessly stolen from Carter's plot.network.default code (sans z).
   xrng<-diff(xlim)          #Force scale to be symmetric
   yrng<-diff(ylim)
+  if(d3)zrng<-diff(zlim)
+  
   xctr<-(xlim[2]+xlim[1])/2 #Get center of plotting region
   yctr<-(ylim[2]+ylim[1])/2
-  if(xrng<yrng)
-    xlim<-c(xctr-yrng/2,xctr+yrng/2)
-  else
-    ylim<-c(yctr-xrng/2,yctr+xrng/2)
+  if(d3)zctr<-(zlim[2]+zlim[1])/2
+  
+  if(!d3){
+    if(xrng<yrng)
+      xlim<-c(xctr-yrng/2,xctr+yrng/2)
+    else
+      ylim<-c(yctr-xrng/2,yctr+xrng/2)
+  }
   ### End stolen part
   
-  list(xlim=xlim,ylim=ylim)
+  list(xlim=xlim,ylim=ylim,zlim=if(d3)zlim)
 }
 
 ergmm.plotting.vertex.radius<-function(vertex.cex,xylim,object.scale){
   baserad<-min(diff(xylim$xlim),diff(xylim$ylim))*object.scale
   vertex.cex*baserad
+}
+
+tetrahedra3d<-function(x,y=NULL,z=NULL,size=1,color="white",alpha=1,...){
+  xyz<-xyz.coords(x=x,y=y,z=z)
+  n<-length(xyz$x)
+  size<-rep(size,length.out=n)
+  color<-rep(color,length.out=n)
+  alpha<-rep(alpha,length.out=n)
+
+  for(i in 1:n){
+    with(xyz,tetrahedron3d(x[i],y[i],z[i],size=size[i],color=color[i],alpha=alpha[i]))
+  }
+}
+
+tetrahedron3d<-function(x,y=NULL,z=NULL,size=1,...){
+  xyz<-xyz.coords(x=x,y=y,z=z)
+  xyz<-with(xyz,c(x,y,z))/size
+  centroid<-c(1/2,1/(2*sqrt(2)),sqrt(3)/4)
+  
+  v.left<-c(0,0,0)+xyz-centroid
+  v.right<-c(1,0,0)+xyz-centroid
+  v.far<-c(1/2,0,sqrt(3)/2)+xyz-centroid
+  v.top<-c(1/2,1/sqrt(2),sqrt(3)/4)+xyz-centroid
+  triangles3d(rbind(v.left,v.right,v.top,
+                    v.left,v.top,v.far,
+                    v.left,v.right,v.far,
+                    v.top,v.right,v.far)*size,y=NULL,z=NULL,...)
 }
