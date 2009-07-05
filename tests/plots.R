f3c527fe1f4138df7264fa1395534df0ef16e5ec
Pavel N. Krivitsky <pavel@uow.edu.au>
2009-07-05 04:42:54 +0000

    Small tweak to one of the tests.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@1038 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/plots.R
+++ tests/plots.R
@@ -19,7 +19,7 @@ plot(with(samp.fit,simulate(model,par=sample[[1]],prior=prior)))
 
 data(tribes)
 tribes.fit<-ergmm(tribes~latent(d=2,G=3),response="sign.012",family="binomial",fam.par=list(trials=2))
-plot(tribes.fit,edge.col=as.matrix(tribes,"gama",m="a")*3+as.matrix(tribes,"rova",m="a")*2,pie=TRUE)
+plot(tribes.fit,edge.col=as.matrix(tribes,"pos",m="a")*3+as.matrix(tribes,"neg",m="a")*2,pie=TRUE)
 
 data(davis)
 davis.fit<-ergmm(davis~latent(d=2,G=2)+rsociality)
