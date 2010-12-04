e09697200df8b10752c13b638824e0d7260069c0
Pavel Krivitsky <pavel@uow.edu.au>
2010-12-04 07:57:09 +0000

    * Fixed some bugs in BIC calculation that I had introduced. * Updated the ChangeLog.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@1240 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/summary.ergmm.R
+++ R/summary.ergmm.R
@@ -223,11 +223,13 @@ bic.ergmm<-function(object){
     stop("MKL estimates were not computed for this fit.")
   }
 
-  n<-network.size(object[["model"]][["Yg"]])
-  
-  condZRE<-with(object,find.mle(object[["model"]],mkl,given=list(Z=mkl[["Z"]],sender=mkl[["sender"]],receiver=mkl[["receiver"]],sociality=mkl[["sociality"]]),control=object[["control"]]))
+  model<-object[["model"]]
 
-  bic<-with(object[["model"]],list(Y = -2*condZRE[["lpY"]] + (p+n*d + (sender + receiver + sociality)*n )*log(n),
+  n<-network.size(model[["Yg"]])
+  
+  condZRE<-with(object,find.mle(model,mkl,given=list(Z=mkl[["Z"]],sender=mkl[["sender"]],receiver=mkl[["receiver"]],sociality=mkl[["sociality"]]),control=object[["control"]]))
+
+  bic<-with(model,list(Y = -2*condZRE[["lpY"]] + (p+n*d + (sender + receiver + sociality)*n )*log(n),
                               Z =
                               if(d>0){
                                 if(G>0){
@@ -249,7 +251,7 @@ bic.ergmm<-function(object){
                               )
             )
 
-  if(model[["sender"]] || model[["receiver"]] || model[["sociality"]] || G==0)
+  if(model[["sender"]] || model[["receiver"]] || model[["sociality"]] || model[["G"]]==0)
     warning("Theory for BIC has not been developed for random actor (sender, receiver, and sociality) effects. Similarly, it may not be appropriate to use BIC to compare clustered models with the unclustered model. Their use in latentnet is entirely heuristic.")
   
   bic[["overall"]]<-sum(unlist(bic))
