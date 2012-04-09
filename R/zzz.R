b182fccac3622efa65d4544871d0e5ce6a0c7169
Pavel Krivitsky <pavel@uow.edu.au>
2012-04-09 20:14:26 +0000

    * latentnet no longer masks generics gof(), mcmc.diagnostics(), mcmc.diagnostics.default(), and robust.inverse() if ergm package has been loaded and provides them. (The converse is not true.) * Namespace imports have been cleaned up, fixing some masking warnings.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@1499 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,18 @@
 .onAttach <- function(lib, pkg){
   info <- packageDescription("latentnet")
     
-    ## Remember where we loaded this instance of latentnet, so that
-    ## the snowFT slave functions could do the same.
-    assign("path.to.me",file_path_as_absolute(lib),pos="package:latentnet")
-    assign("nlog.double.eps",-log(.Machine[["double.eps"]]),pos="package:latentnet")
+  ## Remember where we loaded this instance of latentnet, so that
+  ## the snowFT slave functions could do the same.
+  assign("path.to.me",file_path_as_absolute(lib),pos="package:latentnet")
+  assign("nlog.double.eps",-log(.Machine[["double.eps"]]),pos="package:latentnet")
+
+  # If the following have already been defined in the ergm package, don't duplicate. Otherwise, assign them.
+  IFNOTEXISTS <- c("robust.inverse","mcmc.diagnostics","mcmc.diagnostics.default","gof")
+  for(fun in IFNOTEXISTS){
+    if(!exists(fun, mode="function")){
+      assign(fun, get(paste('.',fun,sep='')), pos="package:latentnet")
+    }
+  }
 
   packageStartupMessage(paste(
                           "copyright (c) 2003-2009, Pavel N. Krivitsky, Pennsylvania State University\n",
