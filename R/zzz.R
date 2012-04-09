d483f2b1cef3389fe1dffa1bbae9f978d8edfd06
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-04-09 19:06:38 +0000

    Replaced .First.lib with .onAttach and updated it to use R's new functionality.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@1493 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/zzz.R
+++ R/zzz.R
@@ -1,28 +1,18 @@
-.First.lib <- function(lib, pkg){
-    library.dynam("latentnet", pkg, lib)
+.onAttach <- function(lib, pkg){
+  info <- packageDescription("latentnet")
     
     ## Remember where we loaded this instance of latentnet, so that
     ## the snowFT slave functions could do the same.
     assign("path.to.me",file_path_as_absolute(lib),pos="package:latentnet")
     assign("nlog.double.eps",-log(.Machine[["double.eps"]]),pos="package:latentnet")
 
-    ehelp <- library(help="latentnet",lib.loc=lib,character.only=TRUE)[["info"]][[1]]
-    cat(paste(substring(ehelp[4],first=16),"\n",
-              "Version ",substring(ehelp[2],first=16),
-              " created on ",
-              substring(ehelp[3],first=16),".\n", sep=""))
-    
-    cat(paste(
-"copyright (c) 2003-2009, Pavel N. Krivitsky, University of Washington\n",
-"                         Mark S. Handcock, University of Washington\n",
-"                         and others; see LICENSE for the full list of\n",
-"                         contributors.\n"
-              ,sep=""))
-    cat('This package is part of statnet project (http://www.statnetproject.org).\n')
-    cat('For citation information type \'citation("latentnet")\'.\n')
-    cat('Type \'help(package="latentnet")\' to get started.\n')
-}
-
-.Last.lib <- function(libpath){
-  library.dynam.unload("latentnet",libpath)
+  packageStartupMessage(paste(
+                          "copyright (c) 2003-2009, Pavel N. Krivitsky, Pennsylvania State University\n",
+                          "                         Mark S. Handcock, University of California, Los Angeles\n",
+                          "                         and others; see LICENSE for the full list of\n",
+                          "                         contributors.\n",
+                          'Based on "statnet" project software (statnet.org).\n',
+                          'For license and citation information see statnet.org/attribution\n',
+                          'or type citation("ergm").\n', sep="")
+                        )
 }
