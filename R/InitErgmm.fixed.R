b1c2df0f0077ab9aa6301d731b6cbe22ed48cacc
Pavel N. Krivitsky <pavel@uow.edu.au>
2014-01-31 13:06:18 +0000

    latentnet's ergmm() can now use ergm terms in its formulas. * From the end-user perspective, the term is invoked the same way it is in ergm, with optional additional arguments, mean= and var= to specify the prior. * This is facilitated by calling ergmMPLE() and grabbing the covariate matrix it returns. * A number of redundant terms (e.g. nodematch) have been removed. * Terms `loops`, `loopcov`, and `loopfactor`, specifically targeted at self-loops (which ergm() doesn't handle) have been added. Handling of intercepts is a bit more intelligent: * An automatic intercept term is only added if there isn't one in the model already. This includes an edges ergm term for a network with no self-loops. * It is now possible to specify a prior for the intercept term via 1(mean=,var=), intercept(mean=,var=), or Intercept(mean=,var=).
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_research/latentnet/trunk@2086 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/InitErgmm.fixed.R
+++ R/InitErgmm.fixed.R
@@ -1,6 +1,146 @@
+.ergmm.add.fixed<-function(model, X, mean, var, coef.names=NULL, where=c("append","prepend")){
+  where <- match.arg(where)
+  
+  if(length(dim(X))==2) dim(X) <- c(dim(X),1)
+  if(!is.null(coef.names)) dimnames(X) <- list(NULL, NULL, coef.names)
+
+  p <- dim(X)[3]
+  model[["p"]]<-model[["p"]]+p
+  model[["coef.names"]]<-switch(where,
+                                append = c(model[["coef.names"]], dimnames(X)[[3]]),
+                                prepend = c(dimnames(X)[[3]], model[["coef.names"]])
+                                )
+                                
+  model[["prior"]][["beta.mean"]]<-switch(where,
+                                          append = c(model[["prior"]][["beta.mean"]], rep(mean,length.out=p)),
+                                          prepend = c(rep(mean,length.out=p), model[["prior"]][["beta.mean"]])
+                                          )
+  
+  model[["prior"]][["beta.var"]]<-switch(where,
+                                         append = c(model[["prior"]][["beta.var"]], rep(var,length.out=p)),
+                                         prepend = c(rep(var,length.out=p), model[["prior"]][["beta.var"]])
+                                         )
+                                         
+
+  Yg <- model[["Yg"]]
+
+
+  Xtmp <- list()
+  for(i in seq_len(p)){
+    xm <- seldrop(X[,,i,drop=FALSE],3)
+    
+    # If the network is bipartite and the matrix has b1*b2 dimensions,
+    # it needs to be augment to (b1+b2)*(b1+b2):
+    if(is.bipartite(Yg)
+       && all(dim(xm)==c(Yg%n%"bipartite", network.size(Yg)-Yg%n%"bipartite")))
+      xm <- bipartite.augment(xm)
+    
+    Xtmp <- c(Xtmp,list(xm))
+  }
+
+  model[["X"]]<-switch(where,
+                       append = c(model[["X"]],Xtmp),
+                       prepend = c(Xtmp, model[["X"]])
+                       )
+
+  model
+}
+
+.import.ergm.term <-function(model, term.name, ..., mean=0, var=9){
+  Yg<-model[["Yg"]]
+  f <- ~Yg
+  f[[3]] <- as.call(c(list(term.name), ...))
+  if(!is.dyad.independent(f)) warning("Term `", term.name, "` induces dyadic dependence. Likelihood will be effectively replaced by pseudolikelihood.", call.=FALSE)
+  if(has.loops(Yg)) warning("Imported ergm term `", term.name, "` will set its dyadic covariate for self-loops, X[i,i,k], to 0. Use `loopfactor` and `loopcov` to model self-loops.", call.=FALSE)
+  
+  X <- ergmMPLE(f, output="array")$predictor
+  
+  X[c(is.na(X))] <- 0
+
+  .ergmm.add.fixed(model, X, mean, var)
+}
+
+InitErgmm.Intercept<-InitErgmm.intercept<-InitErgmm.1<-function(model, mean=0, var=9){
+  #Check to ensure that we got the right number of arguments
+  if (!(nargs() %in% 1:4))
+    stop(paste("`edges` model term expected between 0 and 2 arguments, got ", 
+                                   nargs() - 1, sep = ""), call. = FALSE)
+
+  .ergmm.add.fixed(model,
+                   matrix(1,network.size(model[["Yg"]]),network.size(model[["Yg"]])),
+                   mean, var,
+                   "(Intercept)")
+}
+
+InitErgmm.loops<-function (model, mean=0, var=9){
+  if(!has.loops(model[["Yg"]]))
+    stop("Self-loop term is  meaningless in a network without self-loops", call.=FALSE)
+
+  #Check to ensure that we got the right number of arguments
+  if (!(nargs() %in% 1:3))
+    stop(paste("`loops` model term expected between 0 and 2 arguments, got ", 
+                                   nargs() - 1, sep = ""), call. = FALSE)
+
+  .ergmm.add.fixed(model, diag(1,network.size(model[["Yg"]]),network.size(model[["Yg"]])), mean, var, "loops")
+}
+
+InitErgmm.loopcov <- function (model, attrname, mean=0, var=9){
+  if(!has.loops(model[["Yg"]]))
+    stop("Self-loop covariates are meaningless in a network without self-loops", call.=FALSE)
+
+  #Check to ensure that we got the right number of arguments
+  if (!(nargs() %in% 2:4))
+    stop(paste("loopcov() model term expected between 1 and 3 arguments, got ", 
+                                   nargs() - 1, sep = ""), call. = FALSE)
+  n<-network.size(model[["Yg"]])
+  x<-model[["Yg"]] %v% attrname
+
+  xm<-diag(x,n,n)
+  cn<-paste("loopcov",attrname,sep=".")
+
+  .ergmm.add.fixed(model, xm, mean, var, cn)
+}
+
+InitErgmm.loopfactor <- function (model, attrname, base=1, mean=0, var=9){
+  if(!has.loops(model[["Yg"]]))
+    stop("Self-loop covariates are meaningless in a network without self-loops", call.=FALSE)
+
+  #Check to ensure that we got the right number of arguments
+  if (!(nargs() %in% 2:4))
+    stop(paste("loopfactor() model term expected between 1 and 3 arguments, got ", 
+                                   nargs() - 1, sep = ""), call. = FALSE)
+
+  n<-network.size(model[["Yg"]])
+
+  if(length(attrname)==1)
+    x<-model[["Yg"]] %v% attrname
+  else
+    do.call(paste, c(lapply(attrname, function(a) get.vertex.attribute(model[["Yg"]], a)), sep="."))
+
+  ls<-sort(unique(x))
+  if(NVL(base,0)!=0){
+    ls <- ls[-base]
+    if(length(ls)==0){
+      warning("`loopfactor` term deleted because contributes no statistics.")
+      return(model)
+    }
+  }
+  
+  mean<-rep(mean,length.out=length(ls))
+  var<-rep(var,length.out=length(ls))
+  for(li in seq_along(ls)){
+    l<-ls[[li]]
+    xm<-diag(x==l,n,n)
+    cn<-paste('loopfactor',paste(attrname,collapse="."),l,sep=".")
+    model <- .ergmm.add.fixed(model, xm, mean[li], var[li], cn)
+  }
+  model
+}
+
 InitErgmm.latentcov<-function (model, x, attrname=NULL,
                                mean=0, var=9) 
 {
+  if(!has.loops(model[["Yg"]])) warning("Term `latentcov` is deprecated for networks without self-loops. Use `edgecov` from package `ergm` instead.")
   #Check to ensure that we got the right number of arguments
   if (!(nargs() %in% 2:5))
     stop(paste("latentcov() model term expected between 1 and 4 arguments, got ", 
@@ -17,63 +157,13 @@ InitErgmm.latentcov<-function (model, x, attrname=NULL,
     cn<-if(!is.null(attrname)) attrname else paste("matrix",length(model[["X"]])+1)
   }
 
-  model[["p"]]<-model[["p"]]+1
-  model[["X"]]<-c(model[["X"]],list(xm))
-  model[["coef.names"]]<-c(model[["coef.names"]], cn)
-
-  model[["prior"]][["beta.mean"]]<-c(model[["prior"]][["beta.mean"]],mean)
-  model[["prior"]][["beta.var"]]<-c(model[["prior"]][["beta.var"]],var)
-
-  model
+  .ergmm.add.fixed(model, xm, mean, var, cn)
 }
-InitErgmm.absdiff<-function (model, attrname,
-                             mean=0, var=9) 
-{
-  #Check to ensure that we got the right number of arguments
-  if (!(nargs() %in% 2:4))
-    stop(paste("absdiff() model term expected between 1 and 3 arguments, got ", 
-                                   nargs() - 1, sep = ""), call. = FALSE)
 
-  x<-model[["Yg"]] %v% attrname
-  if(!is.vector(x)||!is.numeric(x))
-    stop(paste("Attribute for absdiff() must be a numeric vector."))
-  xm<-as.matrix(dist(x))
-  cn<-paste('absdiff',attrname,sep=".")
-
-  InitErgmm.latentcov(model,xm,cn,
-                      mean=mean,var=var)
-}
-InitErgmm.nodematch<-function (model, attrname, diff=FALSE, mean=0, var=9) 
-{
-  #Check to ensure that we got the right number of arguments
-  if (!(nargs() %in% 2:5))
-        stop(paste("nodematch() model term expected between 1 and 4 arguments, got ", 
-                   nargs() - 1, sep = ""), call. = FALSE)
-
-  x<-as.factor(model[["Yg"]] %v% attrname)
-  if(!diff){
-    xm<-as.matrix(dist(as.numeric(x)))==0
-    cn<-paste('nodematch',attrname,sep=".")
-    model<-InitErgmm.latentcov(model,xm,cn,mean=mean,var=var)
-  }else{
-    ls<-levels(x)
-    xm<-as.matrix(dist(as.numeric(x)))==0
-    mean<-rep(mean,length.out=length(ls))
-    var<-rep(var,length.out=length(ls))
-    for(li in 1:length(ls)){
-      l<-ls[[li]]
-      xm.l<-xm
-      xm.l[x!=l,]<-FALSE
-      xm.l[,x!=l]<-FALSE
-      cn<-paste('nodematch',attrname,l,sep=".")
-      model<-InitErgmm.latentcov(model,xm.l,cn,mean=mean[li],var=var[li])
-    }
-  }
-  
-  model
-}
 InitErgmm.sendercov<-function (model, attrname, force.factor=FALSE, mean=0, var=9) 
 {
+  if(!has.loops(model[["Yg"]])) warning("Term `sendercov` is deprecated for networks without self-loops. Use `nodeocov`, `nodecov`, `nodeofactor`, or `nodefactor` from package `ergm` instead.")
+
   #Check to ensure that we got the right number of arguments
   if (!(nargs() %in% 2:5))
     stop(paste("sendercov() model term expected between 1 and 4 arguments, got ", 
@@ -87,7 +177,7 @@ InitErgmm.sendercov<-function (model, attrname, force.factor=FALSE, mean=0, var=
     # Numeric covariate.
     xm<-matrix(x,n,n,byrow=FALSE)
     cn<-paste("sendercov",attrname,sep=".")
-    model<-InitErgmm.latentcov(model,xm,cn,mean=mean,var=var)
+    model <- .ergmm.add.fixed(model,xm,mean,var,cn)
   }else{
     # Factor covariate.
     x<-as.factor(x)
@@ -98,13 +188,15 @@ InitErgmm.sendercov<-function (model, attrname, force.factor=FALSE, mean=0, var=
       l<-ls[[li]]
       xm<-matrix(x==l,n,n,byrow=FALSE)
       cn<-paste('sendercov',attrname,l,sep=".")
-      model<-InitErgmm.latentcov(model,xm,cn,mean=mean[li],var=var[li])
+      model <- .ergmm.add.fixed(model, xm, mean[li], var[li], cn)
     }
   }
   model
 }
 InitErgmm.receivercov<-function (model, attrname, force.factor=FALSE, mean=0, var=9) 
 {
+  if(!has.loops(model[["Yg"]])) warning("Term `receivercov` is deprecated for networks without self-loops. Use `nodeicov`, `nodecov`, `nodeifactor`, or `nodefactor` from package `ergm` instead.")
+
   #Check to ensure that we got the right number of arguments
   if (!(nargs() %in% 2:5))
     stop(paste("receivercov() model term expected between 1 and 3 arguments, got ", 
@@ -118,7 +210,7 @@ InitErgmm.receivercov<-function (model, attrname, force.factor=FALSE, mean=0, va
     # Numeric covariate.
     xm<-matrix(x,n,n,byrow=TRUE)
     cn<-paste("receivercov",attrname,sep=".")
-    model<-InitErgmm.latentcov(model,xm,cn,mean=mean,var=var)
+    model <- .ergmm.add.fixed(model,xm,mean,var,cn)
   }else{
     # Factor covariate.
     x<-as.factor(x)
@@ -129,13 +221,16 @@ InitErgmm.receivercov<-function (model, attrname, force.factor=FALSE, mean=0, va
       l<-ls[[li]]
       xm<-matrix(x==l,n,n,byrow=TRUE)
       cn<-paste('receivercov',attrname,l,sep=".")
-      model<-InitErgmm.latentcov(model,xm,cn,mean=mean[li],var=var[li])
+      model <- .ergmm.add.fixed(model, xm, mean[li], var[li], cn)
     }
   }
   model
 }
+
 InitErgmm.socialitycov<-function (model, attrname, force.factor=FALSE, mean=0, var=9) 
 {
+  if(!has.loops(model[["Yg"]])) warning("Term `receivercov` is deprecated for networks without self-loops. Use `nodeicov`, `nodecov`, `nodeifactor`, or `nodefactor` from package `ergm` instead.")
+
   #Check to ensure that we got the right number of arguments
   if (!(nargs() %in% 2:5))
     stop(paste("socialitycov() model term expected between 1 and 4 arguments, got ", 
@@ -147,7 +242,7 @@ InitErgmm.socialitycov<-function (model, attrname, force.factor=FALSE, mean=0, v
     # Numeric covariate.
     xm<-matrix(x,n,n,byrow=FALSE)+matrix(x,n,n,byrow=TRUE)
     cn<-paste("socialitycov",attrname,sep=".")
-    model<-InitErgmm.latentcov(model,xm,cn,mean=mean,var=var)
+    model <- .ergmm.add.fixed(model,xm,mean,var,cn)
   }else{
     # Factor covariate.
     x<-as.factor(x)
@@ -158,7 +253,7 @@ InitErgmm.socialitycov<-function (model, attrname, force.factor=FALSE, mean=0, v
       l<-ls[[li]]
       xm<-matrix(x==l,n,n,byrow=FALSE)+matrix(x==l,n,n,byrow=TRUE)
       cn<-paste('socialitycov',attrname,l,sep=".")
-      model<-InitErgmm.latentcov(model,xm,cn,mean=mean[li],var=var[li])
+      model <- .ergmm.add.fixed(model, xm, mean[li], var[li], cn)
     }
   }
   model
