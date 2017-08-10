f4a2cf4e5471ebcbf11ab06bcb5d81394593b416
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-08-10 11:43:04 +1000

    Documented the directed covariate bug.

--- R/zzz.R
+++ R/zzz.R
@@ -11,5 +11,7 @@
 
 .onAttach <- function(lib, pkg){
   sm <- statnetStartupMessage("latentnet",c("statnet"),FALSE)
-  if(!is.null(sm)) packageStartupMessage(sm,"NOTE: BIC calculation prior to latentnet 2.7.0 had a bug in the calculation of the effective number of parameters. See help(summary.ergmm) for details.")
+  if(!is.null(sm)) packageStartupMessage(sm,
+                                         "NOTE: BIC calculation prior to latentnet 2.7.0 had a bug in the calculation of the effective number of parameters. See help(summary.ergmm) for details.",
+                                         "NOTE: Prior to version 2.8.0, handling of fixed effects for directed networks had a bug. See help(\"ergmm-terms\") for details.")
 }
