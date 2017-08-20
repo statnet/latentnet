01f47ae75760a376a2207fe6129befd762526cea
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-08-20 20:14:46 +1000

    Bumped copyright year and added headers.

--- R/ergmm.initvals.R
+++ R/ergmm.initvals.R
@@ -1,3 +1,12 @@
+#  File R/ergmm.initvals.R in package latentnet, part of the Statnet suite
+#  of packages for network analysis, http://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  http://statnet.org/attribution
+#
+#  Copyright 2003-2017 Statnet Commons
+#######################################################################
 ergmm.initvals <- function(model,user.start,prior,control){
   if(control[["verbose"]]) cat("Generating initial values for MCMC:\n")
   need.to.fit<-list(beta=model[["p"]]>0 && is.null(user.start[["beta"]]), ## beta
--- src/P_alloc.c
+++ src/P_alloc.c
@@ -1,3 +1,12 @@
+/*  File src/P_alloc.c in package latentnet, part of the Statnet suite
+ *  of packages for network analysis, http://statnet.org .
+ *
+ *  This software is distributed under the GPL-3 license.  It is free,
+ *  open source, and has the attribution requirements (GPL Section 7) at
+ *  http://statnet.org/attribution
+ *
+ *  Copyright 2003-2017 Statnet Commons
+ */
 /**********************************************************************/
 /* Routines for memory management similar in functionality to R_alloc */
 /* Used because it's easier to debug memory problems this way.        */
