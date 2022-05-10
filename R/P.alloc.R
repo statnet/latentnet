69334fcc25930d781b4ef5256e7c090d64646aea
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2022-05-10 23:21:02 +1000

    Updated copyright year and headers.

--- R/P.alloc.R
+++ R/P.alloc.R
@@ -1,11 +1,11 @@
-#  File R/P.alloc.R in package latentnet, part of the Statnet suite
-#  of packages for network analysis, https://statnet.org .
+#  File R/P.alloc.R in package latentnet, part of the
+#  Statnet suite of packages for network analysis, https://statnet.org .
 #
 #  This software is distributed under the GPL-3 license.  It is free,
 #  open source, and has the attribution requirements (GPL Section 7) at
-#  https://statnet.org/attribution
+#  https://statnet.org/attribution .
 #
-#  Copyright 2003-2020 Statnet Commons
-#######################################################################
+#  Copyright 2003-2022 Statnet Commons
+################################################################################
 ## Lets the user call P.free.all from R. Only used for debugging.
 P.free.all<-function() invisible(try(.C("P_free_all", PACKAGE="latentnet")))
--- src/procrustes.h
+++ src/procrustes.h
@@ -1,11 +1,11 @@
-/*  File src/procrustes.h in package latentnet, part of the Statnet suite
- *  of packages for network analysis, https://statnet.org .
+/*  File src/procrustes.h in package latentnet, part of the
+ *  Statnet suite of packages for network analysis, https://statnet.org .
  *
  *  This software is distributed under the GPL-3 license.  It is free,
  *  open source, and has the attribution requirements (GPL Section 7) at
- *  https://statnet.org/attribution
+ *  https://statnet.org/attribution .
  *
- *  Copyright 2003-2020 Statnet Commons
+ *  Copyright 2003-2022 Statnet Commons
  */
 #ifndef PROCRUSTES_H
 #define PROCRUSTES_H
